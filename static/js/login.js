201c055c3e33e350416d3490a77af068fef7bcb2
san00719 <san00719@yahoo.com>
2013-09-17 23:56:16 +0530

    Changes added

---
+++ static/js/login.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$("#sign-in").on('click',fb_login);
+});
+
+function fb_login(){
+	FB.login(function(response) {
+
+        if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            //console.log(response); // dump complete info
+            access_token = response.authResponse.accessToken; //get access token
+            user_id = response.authResponse.userID; //get FB UID
+
+            FB.api('/me', function(response) {
+                user_email = response.email; //get user email
+                console.log('Good to see you, ' + response.name + '.');
+          // you can store this data into your database             
+            });
+
+        } else {
+            //user hit cancel button
+            console.log('User cancelled login or did not fully authorize.');
+
+        }
+    }, {
+        scope: 'publish_stream,email'
+    });
+}
